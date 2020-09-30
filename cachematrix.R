37fe1ff7e5310504f3b09120814b72030b50f88f
FernandoDeLaRua <frncdonofrio@gmail.com>
2020-09-30 02:14:55 -0300

    Uploaded finished assignment with funtions makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The program is quite similar to the one in the example, but for 
+## caching an inverse matrix
 
-## Write a short comment describing this function
+## This function stores a matrix and (if asked) its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inversa <- NULL
+    set_matriz <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get_matriz <- function() x
+    set_inversa <- function(solve) inversa <<- solve
+    get_inversa <- function() inversa
+    list(set_matriz = set_matriz,
+         get_matriz = get_matriz,
+         set_inversa = set_inversa,
+         get_inversa = get_inversa)
 }
 
 
-## Write a short comment describing this function
+## and this one caches the inverse matrix (if absent) into an object
+## created by the previous function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inversa <- x$get_inversa()
+    if(!is.null(inversa)){
+        message("gettin' dat cached data")
+        return(inversa)
+    }
+    data <- x$get_matriz()
+    inversa <- solve(data,...)
+    x$set_inversa(inversa)
+    inversa
 }
+
+## to test them you can run:
+## a <- matrix (c(2,3,3,2), nrow=2)
+## f <- makeCacheMatrix(a)
+## to create an object f containing matrix a and by running:
+## cacheSolve(f)
+## a first time the inverse matrix is cached in f$get_inversa(). Subsequent
+## uses return the cached value.
